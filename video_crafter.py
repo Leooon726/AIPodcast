ab2dd0be3dc8682e91bf637718148e5c579831ec
Leon <q5210201@qq.com>
2024-10-20 19:01:56 +0800

    fadeout

--- video_crafter.py
+++ video_crafter.py
@@ -5,6 +5,9 @@ from moviepy.config import change_settings
 from moviepy.video.fx.all import speedx
 from PIL import Image
 import os
+from moviepy.editor import TextClip, CompositeVideoClip
+from moviepy.video.tools.subtitles import SubtitlesClip
+from moviepy.editor import ColorClip
 
 # Specify the path to the ImageMagick binary
 change_settings({"IMAGEMAGICK_BINARY": r"C:\Program Files\ImageMagick-7.1.1-Q16-HDRI\magick.exe"})
@@ -21,9 +24,16 @@ class VideoCrafter():
     output_path: 
     audio_fadeout_duration: 
     bgm_volume:
+    subtitle_config: {
+        font:
+        fontsize:
+        color:
+        y_position: 0.9
+    }
     clips: [
         dict{
             audio_path, # -1 for silence
+            audio_speed,
             key_frame_path,
             frame_size: {
                 width: 
@@ -32,7 +42,7 @@ class VideoCrafter():
             },
             duration,
             transition_pause_time,
-            audio_speed
+            subtitle_text,
         }
         ]
     }
@@ -49,19 +59,46 @@ class VideoCrafter():
         self.config = config
 
     def create_video(self):
-        clips = []
-        for clip_config in self.clips:
-            clips.append(self.create_clip(clip_config))
-        final_video = concatenate_videoclips(clips)
+        # create an empty video clip with the size of the output video
+        canvas_clip = ColorClip(size=(self.width, self.height), color=(255, 255, 255, 0)).set_duration(self.config.get('duration', 0))
         
+        foreground_clips = [canvas_clip]
+        clip_info_dicts = []
+        for clip_config in self.clips:
+            video_clip, clip_info_dict = self.create_clip(clip_config)
+            video_clip = video_clip.set_position(("center", "center")).resize(height=self.height)
+            foreground_clips.append(video_clip)
+            clip_info_dicts.append(clip_info_dict)
+        foreground_video = concatenate_videoclips(foreground_clips, method="compose")
+
+        if self.config.get('background_video_path'):
+            background_clip = self.create_background(duration=foreground_video.duration)
+            final_video = CompositeVideoClip([background_clip, foreground_video])
+        else:
+            final_video = foreground_video
+
         if self.bgm_path and self.bgm_path != -1:
             final_video = self.add_bgm(final_video)
-        
-        if self.config.get('background_video_path'):
-            final_video = self.add_background(final_video)
+
+        if self.config.get('subtitle_config'):
+            final_video = self.add_subtitle(final_video, clip_info_dicts, self.config.get('subtitle_config', {}))
 
         # Specify the fps when writing the video file
-        final_video.write_videofile(self.output_path, codec='libx264', audio_codec='aac', fps=24)
+        # final_video.write_videofile(self.output_path, codec="h264_nvenc", audio_codec='aac', fps=24, ffmpeg_params=["-b:v", "5M"])
+        final_video.write_videofile(
+            self.output_path, 
+            codec="h264_nvenc", 
+            audio_codec='aac', 
+            fps=24, 
+            threads=8,  # Use multiple threads for video processing
+            ffmpeg_params=[
+                "-b:v", "5M",  # Adjust the bitrate as needed
+                "-preset", "fast",  # Speed up encoding
+                "-rc", "vbr",  # Variable bitrate control for better performance
+                "-gpu", "0"  # Ensure the GPU is used if multiple are available
+            ]
+        )
+
         print(f"Video created and saved to {self.output_path}")
 
     def create_audio_clip(self, clip_config):
@@ -105,9 +142,12 @@ class VideoCrafter():
         image_name, image_extension = os.path.splitext(os.path.basename(clip_config['key_frame_path']))
         resized_image_name = f"{image_name}_resized{image_extension}"
         resized_image_path = os.path.join(self.output_dir, resized_image_name)
-        self.resize_image(clip_config['key_frame_path'], resized_image_path, (target_width, target_height))
+        self.resize_image(clip_config['key_frame_path'], resized_image_path, (target_width, target_height), ensure_fit=True)
         print(f"Image resized and saved to {resized_image_path}")
+        
+        # Set the opacity of the image clip
         image_clip = ImageClip(resized_image_path).set_duration(image_clip_duration)
+        
         return image_clip
     
     def create_clip(self, clip_config):
@@ -125,12 +165,60 @@ class VideoCrafter():
             audio_clip = audio_clip.subclip(0, clip_duration - transition_pause_time)
 
         # create image clip
-        image_clip = self.create_image_clip(clip_config, clip_duration)
-        
-        video_clip = image_clip.set_audio(audio_clip)
-        return video_clip
+        if clip_config.get('key_frame_path') and clip_config['key_frame_path'] != -1:
+            image_clip = self.create_image_clip(clip_config, clip_duration)
+            video_clip = image_clip.set_audio(audio_clip)
+        else:
+            video_clip = ColorClip(size=(1, 1), color=(0, 0, 0, 0)).set_duration(clip_duration).set_audio(audio_clip).set_opacity(0)
 
-    def add_background(self, video_clip):
+        # Apply fade-out effect to the video clip
+        if clip_config.get('fadeout_duration'):
+            fadeout_duration = clip_config.get('fadeout_duration')
+            video_clip = video_clip.crossfadeout(fadeout_duration)
+
+        clip_info_dict = {
+            'duration': clip_duration,
+            'subtitle_text': clip_config.get('subtitle_text', '')
+        }
+        return video_clip, clip_info_dict
+
+    def add_subtitle(self, video_clip, clip_info_dicts, subtitle_config):
+        font_size = subtitle_config.get('fontsize', 50)
+        font_color = subtitle_config.get('color', 'white')
+        stroke_color = subtitle_config.get('stroke_color', None)
+        stroke_width = subtitle_config.get('stroke_width', 0)
+        background_color = subtitle_config.get('background_color', None)
+        
+        # Define a text generator function for the subtitles
+        def subtitle_generator(txt):
+            txt_clip = TextClip(txt, font='Microsoft-YaHei-Bold-&-Microsoft-YaHei-UI-Bold',
+                                fontsize=font_size,
+                                color=font_color,
+                                stroke_color=stroke_color,
+                                stroke_width=stroke_width,
+                                bg_color=background_color)
+            return txt_clip
+
+        # Create a list of subtitles with start and end times from clip_info_dicts
+        subs = []
+        sub_start_time = 0
+        for clip_info in clip_info_dicts:
+            subs.append(((sub_start_time, sub_start_time + clip_info['duration']), clip_info['subtitle_text']))
+            sub_start_time += clip_info['duration']
+
+        # Create the SubtitlesClip
+        subtitles = SubtitlesClip(subs, subtitle_generator)
+
+        # Overlay the subtitles below the video
+        subtitle_y_position = subtitle_config.get('y_position', 'bottom')
+        # if subtitle_y_position is a float and <1, then it is a percentage of the video height
+        if isinstance(subtitle_y_position, float) and 0 < subtitle_y_position < 1:
+            subtitle_y_position = int(subtitle_y_position * self.height)
+        video_with_subtitles = CompositeVideoClip([video_clip, subtitles.set_position(('center', subtitle_y_position))])
+
+        return video_with_subtitles
+
+    def create_background(self, duration):
         background_path = self.config['background_video_path']
         if background_path.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp', '.gif')):
             with Image.open(background_path) as img:
@@ -139,20 +227,20 @@ class VideoCrafter():
                     self.resize_image(background_path, resized_background_path, (self.width, self.height))
                     background_clip = (
                         ImageClip(resized_background_path)
-                        .set_duration(video_clip.duration)
+                        .set_duration(duration)
                     )
                 else:
                     background_clip = (
                         ImageClip(background_path)
-                        .set_duration(video_clip.duration)
+                        .set_duration(duration)
                     )
         else:
             background_clip = VideoFileClip(background_path)
             if background_clip.size != (self.width, self.height):
                 background_clip = background_clip.resize((self.width, self.height))
-            background_clip = background_clip.set_duration(video_clip.duration)
-        video_clip = CompositeVideoClip([background_clip, video_clip.set_position("center")])
-        return video_clip
+            background_clip = background_clip.set_duration(duration)
+
+        return background_clip
 
     def add_bgm(self, video_clip):
         bgm_clip = AudioFileClip(self.bgm_path).volumex(self.bgm_volume)
@@ -162,12 +250,27 @@ class VideoCrafter():
         return final_video
 
     @staticmethod
-    def resize_image(input_path, output_path, target_size):
+    def resize_image(input_path, output_path, target_size, ensure_fit=True):
         if os.path.exists(output_path):
             print(f"File {output_path} already exists")
             return
         with Image.open(input_path) as img:
-            img = img.resize(target_size, Image.LANCZOS)  # Resize the image to the target size
+            original_width, original_height = img.size
+            target_width, target_height = target_size
+
+            if ensure_fit:
+                width_ratio = target_width / original_width
+                height_ratio = target_height / original_height
+                scale_factor = min(width_ratio, height_ratio)
+            else:
+                width_ratio = target_width / original_width
+                height_ratio = target_height / original_height
+                scale_factor = max(width_ratio, height_ratio)
+
+            new_width = int(original_width * scale_factor)
+            new_height = int(original_height * scale_factor)
+
+            img = img.resize((new_width, new_height), Image.LANCZOS)
             img.save(output_path)
 
 
@@ -188,7 +291,7 @@ def change_audio_speed_without_pitch(audio_path, speed_factor, output_dir=None):
     
     return output_path
 
-def test_main():
+def test_main_1():
     config = {
         'height': 1920,
         'width': 1080,
@@ -204,7 +307,8 @@ def test_main():
                 'frame_size': {'width': -1, 'height': 508},
                 'duration': -1,
                 'transition_pause_time': 1,
-                'audio_speed': 1.2
+                'audio_speed': 1.2,
+                'subtitle_text': 'This is a test subtitle'
             },
             {
                 'audio_path': 'D:\Study\AIAgent\AIEnglishLearning\output\cluster0\\0_output_audio.wav',
@@ -219,6 +323,47 @@ def test_main():
     video_crafter = VideoCrafter(config)
     video_crafter.create_video()
 
+def test_main_2():
+    config = {
+        'height': 1080,
+        'width': 1920,
+        'bgm_path': 'D:\Study\AIAgent\AIEnglishLearning\static_materials\scott-buckley-reverie(chosic.com).mp3',
+        # 'background_video_path': 'D:\Study\AIAgent\AIPodcast\\assets\\raining_window_10min.mp4',
+        'background_video_path': 'D:\Study\AIAgent\AIPodcast\\assets\\raining_window_img.jpg',
+        'output_path': 'D:\Study\AIAgent\AIPodcast\output\output.mp4',
+        'audio_fadeout_duration': 2,
+        'bgm_volume': 0.5,
+        'subtitle_config': {
+            'y_position': 0.8,
+            'background_color': 'black'
+        },
+        'clips': [
+            {
+                'audio_path': 'D:\Study\AIAgent\AIPodcast\output\episode_test\downloaded_audios\stretched_audios\\2b625210f2124f05808abf75c0fca9ee_stretched.mp3',
+                'key_frame_path': 'D:\Study\AIAgent\AIPodcast\\assets\logo.png',
+                'frame_size': {'width': -1, 'height': 508},
+                'duration': -1,
+                'transition_pause_time': 2,
+                'audio_speed': 1.0,
+                'subtitle_text': 'This is 是低调a test subtitle',
+                'fadeout_duration': 2
+            },
+            {
+                'audio_path': 'D:\Study\AIAgent\AIPodcast\output\episode_test\downloaded_audios\stretched_audios\\1b628e22456941de8392f891aa630d9a_stretched.mp3',
+                # 'key_frame_path': 'D:\Study\AIAgent\AIEnglishLearning\output\cluster0\\1_key_frame_1.jpeg',
+                'key_frame_path': -1,
+                'frame_size': {'width': -1, 'height': 508},
+                'duration': -1,
+                'transition_pause_time': 1,
+                'audio_speed': 1.0,
+                'subtitle_text': 'This is 深度方a test subtitle'
+            },
+        ]
+    }
+
+    video_crafter = VideoCrafter(config)
+    video_crafter.create_video()
+
 def test_resize_image():
     VideoCrafter.resize_image('D:\Study\AIAgent\AIEnglishLearning\static_materials\卡通女生图片.jpeg', 'D:\Study\AIAgent\AIPodcast\output\卡通女生图片_resized.jpeg', (1080, 1920))
 
@@ -226,8 +371,12 @@ def test_change_audio_speed_without_pitch():
     change_audio_speed_without_pitch('D:\Study\AIAgent\AIPodcast\output\\test_input.mp3', 1.2, 'D:\Study\AIAgent\AIPodcast\output')
 
 if __name__ == '__main__':
-    test_main()
+    # test_main_1()
+
+    test_main_2()
 
     # test_resize_image()
 
     # test_change_audio_speed_without_pitch()
+
+
