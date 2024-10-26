5a8cbfafaa573a93d3def07732be8c4ae62370d6
Leon <q5210201@qq.com>
2024-10-26 20:36:15 +0800

    pure audio

--- podcast_data_preparation.py
+++ podcast_data_preparation.py
@@ -12,7 +12,7 @@ class PodcastDataJsonReader:
 
 
 class PodcastDataPreparation:
-    def __init__(self, json_path, output_dir, video_width, video_height, key_frame_path):
+    def __init__(self, json_path, output_dir, video_width=1920, video_height=1080, key_frame_path=-1):
         self.url_data_list = PodcastDataJsonReader(json_path).read_json()
         self.output_dir = output_dir
         if not os.path.exists(self.output_dir):
@@ -25,26 +25,82 @@ class PodcastDataPreparation:
         return {
             'height': self.video_height,
             'width': self.video_width,
-            # 'bgm_path': 'D:\Study\AIAgent\AIEnglishLearning\static_materials\scott-buckley-reverie(chosic.com).mp3',
-            'bgm_path': -1,
+            'bgm_path': 'D:\Study\AIAgent\AIPodcast\\assets\lounge_jazz.mp3',
+            'background_video_path': 'D:\Study\AIAgent\AIPodcast\\assets\\raining_window_10min.mp4',
             'output_path': os.path.join(self.output_dir, 'output.mp4'),
             'audio_fadeout_duration': 2,
-            'bgm_volume': 0.5,
+            'bgm_volume': 0.3,
+            'subtitle_config': {
+                'y_position': 0.8,
+                'background_color': 'black'
+            },
             'clips': []
         }
 
     def prepare_data(self):
         video_info_config = self.get_basic_video_info()
+        # Cover
+        cover_clip_info = {
+            'audio_path': -1,
+            'key_frame_path': 'D:\Study\AIAgent\AIPodcast\\assets\cover.png',
+            'frame_size': {'width': 1920, 'height': 1080, 'unit': 'pixel'},
+            'duration': 1,
+            'fadeout_duration': 0,
+            'transition_pause_time': 0.0,
+            'subtitle_text': ''
+        }
+        video_info_config['clips'].append(cover_clip_info)
+        # Opening
+        opening_clip_info = {
+            'audio_path': -1,
+            'key_frame_path': 'D:\Study\AIAgent\AIPodcast\\assets\logo.png',
+            'frame_size': {'width': -1, 'height': 0.5, 'unit': 'ratio'},
+            'duration': 3,
+            'fadeout_duration': 2,
+            'transition_pause_time': 0.3,
+            'subtitle_text': ''
+        }
+        video_info_config['clips'].append(opening_clip_info)
+        # Body
         for item in self.url_data_list:
             clip_info = {
                 'audio_path': self.download_audio(item['audio_url']),
                 'key_frame_path': self.key_frame_path,
                 'duration': -1,
-                'transition_pause_time': 0.5,
-                'audio_speed': 1.1
+                'transition_pause_time': 0.3,
+                'audio_speed': 1.1,
+                'subtitle_text': self.get_subtitle(item['sentence'])
             }
             video_info_config['clips'].append(clip_info)
         return video_info_config
+
+    def get_basic_pure_audio_info(self):
+        return {
+            'bgm_path': 'D:\Study\AIAgent\AIPodcast\\assets\lounge_jazz.mp3',
+            'output_path': os.path.join(self.output_dir, 'output.mp3'),
+            'audio_fadeout_duration': 2,
+            'bgm_volume': 0.3,
+            'clips': []
+        }
+
+    def prepare_pure_audio_data(self):
+        pure_audio_info_config = self.get_basic_pure_audio_info()
+        # Opening
+        opening_clip_info = {
+            'audio_path': -1,
+            'duration': 2,
+        }
+        pure_audio_info_config['clips'].append(opening_clip_info)
+        # Body
+        for item in self.url_data_list:
+            clip_info = {
+                'audio_path': self.download_audio(item['audio_url']),
+                'duration': -1,
+                'transition_pause_time': 0.3,
+                'audio_speed': 1.1,
+            }
+            pure_audio_info_config['clips'].append(clip_info)
+        return pure_audio_info_config
 
     def download_audio(self, url):
         output_dir = os.path.join(self.output_dir, 'downloaded_audios')
@@ -64,6 +120,17 @@ class PodcastDataPreparation:
         print(f"Downloaded {url} to {local_filename}")
         return local_filename
 
+    def get_subtitle(self, sentence):
+        '''input: 原来如此。（恍然大悟）啊，我懂了。
+        output: 原来如此。啊，我懂了。
+        sometimes there is no '（' or '）' in the sentence, so we need to handle this situation
+        '''
+        while '（' in sentence and '）' in sentence:
+            start = sentence.index('（')
+            end = sentence.index('）') + 1
+            sentence = sentence[:start] + sentence[end:]
+        return sentence
+
 def test_PodcastDataJsonReader():
     reader = PodcastDataJsonReader('D:\Study\AIAgent\AIPodcast\output\podcast_data.json')
     data = reader.read_json()
@@ -71,9 +138,14 @@ def test_PodcastDataJsonReader():
         print(item)
 
 def test_PodcastDataPreparation():
-    preparation = PodcastDataPreparation('D:\Study\AIAgent\AIPodcast\output\podcast_data.json', 'D:\Study\AIAgent\AIPodcast\output\\episode_test')
+    preparation = PodcastDataPreparation('D:\Study\AIAgent\AIPodcast\output\episode_test\\test_long.json', 'D:\Study\AIAgent\AIPodcast\output\\episode_test')
     print(preparation.prepare_data())
+
+def test_get_subtitle():
+    preparation = PodcastDataPreparation('D:\Study\AIAgent\AIPodcast\output\episode_test\\test_long.json', 'D:\Study\AIAgent\AIPodcast\output\\episode_test')
+    print(preparation.get_subtitle("原来（思考片刻）“我要做”，就是我们该做却总是拖延的事情，对吧？"))
 
 if __name__ == '__main__':
     # test_PodcastDataJsonReader()
-    test_PodcastDataPreparation()
+    # test_PodcastDataPreparation()
+    test_get_subtitle()
--- video_crafter.py
+++ video_crafter.py
@@ -1,4 +1,4 @@
-from moviepy.editor import AudioFileClip, ImageClip, concatenate_videoclips, concatenate_audioclips, CompositeAudioClip, VideoFileClip, CompositeVideoClip
+from moviepy.editor import AudioFileClip, ImageClip, concatenate_videoclips, concatenate_audioclips, CompositeAudioClip, VideoFileClip, CompositeVideoClip, vfx
 from moviepy.audio.AudioClip import AudioArrayClip
 import numpy as np
 from moviepy.config import change_settings
@@ -9,9 +9,49 @@ from moviepy.editor import TextClip, CompositeVideoClip
 from moviepy.video.tools.subtitles import SubtitlesClip
 from moviepy.editor import ColorClip
 import time
+import re
+import math
+import numpy
 
 # Specify the path to the ImageMagick binary
 change_settings({"IMAGEMAGICK_BINARY": r"C:\Program Files\ImageMagick-7.1.1-Q16-HDRI\magick.exe"})
+
+def zoom_effect(clip, zoom_ratio_per_second=0.04, zoom_type='in'):
+    def effect(get_frame, t):
+        img = Image.fromarray(get_frame(t))
+        base_size = img.size
+
+        if zoom_type == 'in':
+            scale_factor = 1 + (zoom_ratio_per_second * t)
+        elif zoom_type == 'out':
+            scale_factor = 1 + (zoom_ratio_per_second * (1 - t))
+        else:
+            raise ValueError("zoom_type must be either 'in' or 'out'")
+
+        new_size = [
+            math.ceil(img.size[0] * scale_factor),
+            math.ceil(img.size[1] * scale_factor)
+        ]
+
+        # The new dimensions must be even.
+        new_size[0] = new_size[0] + (new_size[0] % 2)
+        new_size[1] = new_size[1] + (new_size[1] % 2)
+
+        img = img.resize(new_size, Image.LANCZOS)
+
+        x = math.ceil((new_size[0] - base_size[0]) / 2)
+        y = math.ceil((new_size[1] - base_size[1]) / 2)
+
+        img = img.crop([
+            x, y, new_size[0] - x, new_size[1] - y
+        ]).resize(base_size, Image.LANCZOS)
+
+        result = numpy.array(img)
+        img.close()
+
+        return result
+
+    return clip.fl(effect).set_position(("center", "center"))
 
 class VideoCrafter():
     '''
@@ -91,15 +131,18 @@ class VideoCrafter():
         audio_clip = self._create_final_audio()
         audio_clip.write_audiofile(self.output_path, codec='mp3', fps=44100)
 
-    def create(self):
+    def create(self, use_fast_mode=True):
         # check if the output_path is a pure audio file
         if self.output_path.lower().endswith(('.mp3', '.wav', '.m4a')):
             self.create_pure_audio()
         else:
-            self.create_video_fast()
+            if use_fast_mode:
+                self.create_video_fast()
+            else:
+                self.create_video()
 
     def create_video_fast(self):
-        '''This implementation could be two times faster than the create_video method'''
+        '''This implementation could be two times faster than the create_video method, if there are not many key frames.'''
         cur_start_time = 0
         for clip_config in self.clips_config:
             cur_clip_info_dict = {}
@@ -133,7 +176,7 @@ class VideoCrafter():
             video_clip = clip_info_dict['image_clip']
             if video_clip is not None:
                 video_clip = video_clip.set_start(clip_info_dict['start_time'])
-                video_clip = video_clip.set_position(("center", "center")).resize(height=self.height)
+                # video_clip = video_clip.set_position(("center", "center"))
                 final_video = CompositeVideoClip([final_video, video_clip])
             
         final_video = final_video.set_audio(final_audio)
@@ -206,7 +249,7 @@ class VideoCrafter():
     def create_audio_clip(self, clip_config):
         audio_speed = clip_config.get('audio_speed', 1.0)
         transition_pause_time = clip_config.get('transition_pause_time', 0)
-        if clip_config['audio_path'] != -1:
+        if clip_config.get('audio_path') and clip_config['audio_path'] != -1:
             # Change audio speed without altering pitch
             if audio_speed != 1.0:
                 modified_audio_path = change_audio_speed_without_pitch(clip_config['audio_path'], audio_speed)
@@ -233,8 +276,12 @@ class VideoCrafter():
 
         # Resize image according to frame_size
         frame_size = clip_config.get('frame_size', {'width': self.width, 'height': self.height})
-        target_width = frame_size.get('width', self.width)
-        target_height = frame_size.get('height', self.height)
+        if frame_size.get('unit', '') == 'ratio':
+            target_width = int(frame_size['width'] * self.width) if frame_size['width'] != -1 else -1
+            target_height = int(frame_size['height'] * self.height) if frame_size['height'] != -1 else -1
+        else:
+            target_width = frame_size.get('width', self.width)
+            target_height = frame_size.get('height', self.height)
         
         if target_width == -1 and target_height != -1:
             with Image.open(clip_config['key_frame_path']) as img:
@@ -248,14 +295,35 @@ class VideoCrafter():
             target_width = self.width
             target_height = self.height
         
-        image_name, image_extension = os.path.splitext(os.path.basename(clip_config['key_frame_path']))
-        resized_image_name = f"{image_name}_resized{image_extension}"
-        resized_image_path = os.path.join(self.output_dir, resized_image_name)
-        self.resize_image(clip_config['key_frame_path'], resized_image_path, (target_width, target_height), ensure_fit=True)
-        print(f"Image resized and saved to {resized_image_path}")
-        
+        # Check if the image needs to be resized
+        with Image.open(clip_config['key_frame_path']) as img:
+            original_width, original_height = img.size
+
+        if original_width != target_width or original_height != target_height:
+            image_name, image_extension = os.path.splitext(os.path.basename(clip_config['key_frame_path']))
+            resized_image_name = f"{image_name}_resized{image_extension}"
+            resized_image_path = os.path.join(self.output_dir, resized_image_name)
+            self.resize_image(clip_config['key_frame_path'], resized_image_path, (target_width, target_height), ensure_fit=True)
+            print(f"Image resized and saved to {resized_image_path}")
+        else:
+            resized_image_path = clip_config['key_frame_path']
+            print(f"Image does not need resizing. Using original image: {resized_image_path}")
         # Set the opacity of the image clip
         image_clip = ImageClip(resized_image_path).set_duration(image_clip_duration)
+
+        # Add movement to the image clip
+        if clip_config.get('movement'):
+            movement = clip_config['movement']
+            if movement['type'] == 'pan':
+                video_width = self.width
+                image_clip = image_clip.set_position(lambda t: (t * (video_width / image_clip_duration), 'center'))
+            elif movement['type'] == 'zoom':
+                start_resize_ratio = movement.get('start_resize_ratio', 1)
+                end_resize_ratio = movement.get('end_resize_ratio', 1.5)
+                zoom_ratio_per_second = (end_resize_ratio - start_resize_ratio) / image_clip_duration
+                # Determine the zoom type based on the start and end resize ratios
+                zoom_type = 'in' if start_resize_ratio < end_resize_ratio else 'out'
+                image_clip = zoom_effect(image_clip, zoom_ratio_per_second=zoom_ratio_per_second, zoom_type=zoom_type)
 
         # Apply fade-out effect to the video clip
         if clip_config.get('fadeout_duration'):
@@ -275,7 +343,7 @@ class VideoCrafter():
             clip_duration = audio_clip.duration + transition_pause_time
 
         # trim audio clip to match the video clip duration
-        if clip_config['audio_path'] != -1:
+        if clip_config.get('audio_path') and clip_config['audio_path'] != -1:
             audio_clip = audio_clip.subclip(0, clip_duration - transition_pause_time)
 
         # create image clip
@@ -296,13 +364,57 @@ class VideoCrafter():
         }
         return video_clip, clip_info_dict
 
-    def add_subtitle(self, video_clip, clip_info_dicts, subtitle_config):
+    def split_subtitle_text(self, subtitle_text, text_length_limit=25):
+        # Split the text based on punctuation
+        punctuation = "，。；！？"
+        pieces = re.split(f'([{punctuation}])', subtitle_text)
+        result = []
+        current_piece = ""
+        
+        # Merge short pieces based on text_length_limit
+        for piece in pieces:
+            if piece in punctuation:
+                current_piece += piece
+                if len(current_piece) > text_length_limit:
+                    result.append(current_piece)
+                    current_piece = ""
+            else:
+                if len(current_piece) + len(piece) > text_length_limit:
+                    result.append(current_piece)
+                    current_piece = piece
+                else:
+                    current_piece += piece
+        
+        if current_piece:
+            result.append(current_piece)
+        
+        # Split merged pieces that are longer than text_length_limit by determining the number of pieces needed, then split accordingly.
+        final_result = []
+        for piece in result:
+            if len(piece) > text_length_limit:
+                piece_num = (len(piece) + text_length_limit - 1) // text_length_limit
+                split_length = len(piece) // piece_num
+                for i in range(piece_num - 1):
+                    final_result.append(piece[i * split_length:(i + 1) * split_length])
+                final_result.append(piece[(piece_num - 1) * split_length:])
+            else:
+                final_result.append(piece)
+        
+        return final_result
+
+    def add_subtitle(self, video_clip, clip_info_dicts, subtitle_config, text_length_limit=25):
         font_size = subtitle_config.get('fontsize', 50)
         font_color = subtitle_config.get('color', 'white')
         stroke_color = subtitle_config.get('stroke_color', None)
         stroke_width = subtitle_config.get('stroke_width', 0)
         background_color = subtitle_config.get('background_color', None)
         
+        def append_subtitle(subs, subtitle_text, duration, sub_start_time):
+            if subtitle_text == '':
+                return subs
+            subs.append(((sub_start_time, sub_start_time + duration), subtitle_text))
+            return subs
+
         # Define a text generator function for the subtitles
         def subtitle_generator(txt):
             txt_clip = TextClip(txt, font='Microsoft-YaHei-Bold-&-Microsoft-YaHei-UI-Bold',
@@ -313,13 +425,30 @@ class VideoCrafter():
                                 bg_color=background_color)
             return txt_clip
 
+        def split_long_subtitle(subs, subtitle_text, duration, sub_start_time, text_length_limit):
+            splited_subtitle_pieces = self.split_subtitle_text(subtitle_text, text_length_limit)
+            total_length = len(subtitle_text)
+            duration_of_a_character = duration / total_length
+            for piece_text in splited_subtitle_pieces:
+                piece_duration = duration_of_a_character * len(piece_text)
+                start_time = sub_start_time
+                end_time = start_time + piece_duration
+                subs.append(((start_time, end_time), piece_text))
+                sub_start_time = end_time
+            return sub_start_time, subs
+
         # Create a list of subtitles with start and end times from clip_info_dicts
         subs = []
         sub_start_time = 0
         for clip_info in clip_info_dicts:
-            subs.append(((sub_start_time, sub_start_time + clip_info['duration']), clip_info['subtitle_text']))
-            sub_start_time += clip_info['duration']
-
+            subtitle_text = clip_info['subtitle_text']
+            if len(subtitle_text) > text_length_limit:
+                sub_start_time, subs = split_long_subtitle(subs, subtitle_text, clip_info['duration'], sub_start_time, text_length_limit)
+            else:
+                subs = append_subtitle(subs, subtitle_text, clip_info['duration'], sub_start_time)
+                # subs.append(((sub_start_time, sub_start_time + clip_info['duration']), subtitle_text))
+                sub_start_time += clip_info['duration']
+        print(subs)
         # Create the SubtitlesClip
         subtitles = SubtitlesClip(subs, subtitle_generator)
 
@@ -410,8 +539,8 @@ def test_create_video_fast():
         'height': 1080,
         'width': 1920,
         'bgm_path': 'D:\Study\AIAgent\AIEnglishLearning\static_materials\scott-buckley-reverie(chosic.com).mp3',
-        # 'background_video_path': 'D:\Study\AIAgent\AIPodcast\\assets\\raining_window_10min.mp4',
-        'background_video_path': 'D:\Study\AIAgent\AIPodcast\\assets\\raining_window_img.jpg',
+        'background_video_path': 'D:\Study\AIAgent\AIPodcast\\assets\\raining_window_10min.mp4',
+        # 'background_video_path': 'D:\Study\AIAgent\AIPodcast\\assets\\raining_window_img.jpg',
         'output_path': 'D:\Study\AIAgent\AIPodcast\output\output.mp4',
         'audio_fadeout_duration': 2,
         'bgm_volume': 0.5,
@@ -423,12 +552,12 @@ def test_create_video_fast():
             {
                 'audio_path': 'D:\Study\AIAgent\AIPodcast\output\episode_test\downloaded_audios\stretched_audios\\2b625210f2124f05808abf75c0fca9ee_stretched.mp3',
                 'key_frame_path': 'D:\Study\AIAgent\AIPodcast\\assets\logo.png',
-                'frame_size': {'width': -1, 'height': 508},
+                'frame_size': {'width': -1, 'height': 0.5, 'unit': 'ratio'},
                 'duration': -1,
                 'transition_pause_time': 2,
                 'audio_speed': 1.0,
                 'subtitle_text': 'This is 是低调a test subtitle',
-                'fadeout_duration': 2
+                'fadeout_duration': 2,
             },
             {
                 'audio_path': 'D:\Study\AIAgent\AIPodcast\output\episode_test\downloaded_audios\stretched_audios\\1b628e22456941de8392f891aa630d9a_stretched.mp3',
@@ -446,6 +575,46 @@ def test_create_video_fast():
     video_crafter = VideoCrafter(config)
     video_crafter.create()
 
+def test_movement():
+    config = {
+        'height': 1080,
+        'width': 1920,
+        'bgm_path': 'D:\Study\AIAgent\AIEnglishLearning\static_materials\scott-buckley-reverie(chosic.com).mp3',
+        'output_path': 'D:\Study\AIAgent\AIPodcast\output\output.mp4',
+        'audio_fadeout_duration': 2,
+        'bgm_volume': 0.5,
+        'subtitle_config': {
+            'y_position': 0.8,
+            'background_color': 'black'
+        },
+        'clips': [
+            {
+                'audio_path': 'D:\Study\AIAgent\AIPodcast\output\episode_test\downloaded_audios\stretched_audios\\2b625210f2124f05808abf75c0fca9ee_stretched.mp3',
+                'key_frame_path': 'D:\Study\AIAgent\AIPodcast\\assets\\raining_window_img.jpg',
+                'frame_size': {'width': -1, 'height': 1, 'unit': 'ratio'},
+                'duration': -1,
+                'transition_pause_time': 2,
+                'audio_speed': 1.0,
+                'subtitle_text': 'This is 是低调a test subtitle',
+                'fadeout_duration': 2,
+                'movement': {'type': 'zoom', 'start_resize_ratio': 1.2, 'end_resize_ratio': 1.}
+                # 'movement': {'type': 'pan'}
+            },
+            # {
+            #     'audio_path': 'D:\Study\AIAgent\AIPodcast\output\episode_test\downloaded_audios\stretched_audios\\1b628e22456941de8392f891aa630d9a_stretched.mp3',
+            #     'key_frame_path': 'D:\Study\AIAgent\AIPodcast\\assets\\raining_window_img.jpg',
+            #     'frame_size': {'width': 1, 'height': -1, 'unit': 'ratio'},
+            #     'duration': -1,
+            #     'transition_pause_time': 1,
+            #     'audio_speed': 1.0,
+            #     'subtitle_text': 'This is 深度方a test subtitle',
+            #     'movement': 'zoom_out'
+            # },
+        ]
+    }
+
+    video_crafter = VideoCrafter(config)
+    video_crafter.create()
 
 def test_create_pure_audio():
     config = {
@@ -528,8 +697,56 @@ def test_resize_image():
 def test_change_audio_speed_without_pitch():
     change_audio_speed_without_pitch('D:\Study\AIAgent\AIPodcast\output\\test_input.mp3', 1.2, 'D:\Study\AIAgent\AIPodcast\output')
 
+def test_subtitle_split():
+    config = {
+        'height': 1080,
+        'width': 1920,
+        'bgm_path': 'D:\Study\AIAgent\AIEnglishLearning\static_materials\scott-buckley-reverie(chosic.com).mp3',
+        'background_video_path': 'D:\Study\AIAgent\AIPodcast\\assets\\raining_window_10min.mp4',
+        # 'background_video_path': 'D:\Study\AIAgent\AIPodcast\\assets\\raining_window_img.jpg',
+        'output_path': 'D:\Study\AIAgent\AIPodcast\output\output.mp4',
+        'audio_fadeout_duration': 2,
+        'bgm_volume': 0.5,
+        'subtitle_config': {
+            'y_position': 0.8,
+            'background_color': 'black'
+        },
+        'clips': [
+            {
+                'audio_path': 'D:\Study\AIAgent\AIPodcast\output\episode_test\downloaded_audios\stretched_audios\\2b625210f2124f05808abf75c0fca9ee_stretched.mp3',
+                'key_frame_path': 'D:\Study\AIAgent\AIPodcast\\assets\logo.png',
+                'frame_size': {'width': -1, 'height': 0.5, 'unit': 'ratio'},
+                'duration': -1,
+                'transition_pause_time': 2,
+                'audio_speed': 1.0,
+                # 'subtitle_text': 'This is 是低调a test subtitle',
+                'subtitle_text': '',
+                'fadeout_duration': 2
+            },
+            {
+                'audio_path': 'D:\Study\AIAgent\AIPodcast\output\episode_test\downloaded_audios\stretched_audios\\1b628e22456941de8392f891aa630d9a_stretched.mp3',
+                # 'key_frame_path': 'D:\Study\AIAgent\AIEnglishLearning\output\cluster0\\1_key_frame_1.jpeg',
+                'key_frame_path': -1,
+                'frame_size': {'width': -1, 'height': 508},
+                'duration': -1,
+                'transition_pause_time': 1,
+                'audio_speed': 1.0,
+                'subtitle_text': 'This is 深度方a test subtitle'
+            },
+        ]
+    }
+    video_crafter = VideoCrafter(config)
+    subtitle_text = "大家好，我是一朵。今天我特别好奇，因为我没读过《自控力》这本书因为我没读过《自控力》这本书因为我没读过《自控力》这本书因为我没读过《自控力》这本书，所以想请教乐橙，你能简单介绍一下吗？听说是关于意志力的？"
+    splited_text = video_crafter.split_subtitle_text(subtitle_text)
+    for text in splited_text:
+        print(text, len(text))
+
 if __name__ == '__main__':
-    test_create_video_fast()
+    # test_subtitle_split()
+
+    # test_create_video_fast()
+
+    test_movement()
 
     # test_create_video()
 
@@ -538,6 +755,5 @@ if __name__ == '__main__':
     # test_resize_image()
 
     # test_change_audio_speed_without_pitch()
-
 
 
